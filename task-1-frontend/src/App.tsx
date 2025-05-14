e2e399da058c4ac086d06117fd78e07eaa5d52fb
san-shor <sanjidaewu89@gmail.com>
2025-05-14 19:08:02 +0600

    feat: add GaugeChart component and integrate chart selection in App

--- task-1-frontend/src/App.tsx
+++ task-1-frontend/src/App.tsx
@@ -1,15 +1,51 @@
-
-import './App.css'
-import BarChart from './components/BarChart'
+import BarChart from "./components/BarChart";
+import GaugeChart from "./components/GaugeChart";
+import { Stack, Button } from "@mui/material";
+import {  useState } from "react";
 
 function App() {
+ 
+  const [selectedChart, setSelectedChart] = useState<"bar" | "gauge" | null>(null);
+ 
   
-
+ 
   return (
-    <>
-      <BarChart/>
-    </>
-  )
+  <>
+    <Stack
+      display={"flex"}
+      flexDirection={"row"}
+      gap={2}
+      padding={2}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <Button
+        variant="contained"
+        color="primary"
+        size="large"
+        sx={{ width: "200px" }}
+        onClick={() => setSelectedChart("bar")}
+      >
+        Bar Chart
+      </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        size="large"
+        sx={{ width: "200px" }}
+        onClick={() => setSelectedChart("gauge")}
+      >
+        Gauge Chart
+      </Button>
+       
+    </Stack>
+    
+    
+        {selectedChart === "bar" && <BarChart />}
+        {selectedChart === "gauge" && <GaugeChart />}
+    
+  </>
+  );
 }
 
-export default App
+export default App;
---
+++ task-1-frontend/src/components/GaugeChart.tsx
@@ -0,0 +1,11 @@
+
+
+const GaugeChart = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default GaugeChart
